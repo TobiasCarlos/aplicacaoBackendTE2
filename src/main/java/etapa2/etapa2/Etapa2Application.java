b455098c54503a909a93146dbe38ef9973ce839c
Tobias <tobias@pop-os.localdomain>
2022-05-17 11:31:04 -0300

    first commit

---
+++ src/main/java/etapa2/etapa2/Etapa2Application.java
@@ -0,0 +1,13 @@
+package etapa2.etapa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Etapa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Etapa2Application.class, args);
+	}
+
+}
---
+++ src/test/java/etapa2/etapa2/Etapa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package etapa2.etapa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Etapa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
