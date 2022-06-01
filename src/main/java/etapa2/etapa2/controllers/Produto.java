ec1dac7a958cdebfbac5216577c6b7b4d8e9ffbe
Caio <caionuneso@hotmail.com>
2022-05-31 21:15:50 -0300

    resolvido alguns bugs de tipagem

--- src/main/java/etapa2/etapa2/controllers/Produto.java
+++ src/main/java/etapa2/etapa2/controllers/Produto.java
@@ -4,6 +4,6 @@ public class Produto {
     private string tipo;
     private string nome;
     private string descricao;
-    private string valor;
+    private float valor;
     private Planta planta;
 }
